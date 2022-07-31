94b921030a80dc11a16175e6cdb8a318f446a56a
Mohamed Ziad Alhafez <mohamedziadalhafez@gmail.com>
2022-07-31 02:16:38 +0200

    app module and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MasterLayoutComponent } from './layouts/master-layout/master-layout.component';
+import { PlanATripComponent } from './views/plan-a-trip/plan-a-trip.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: MasterLayoutComponent,
+    children: [
+      {
+        path: "plan-a-trip",
+        component: PlanATripComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SharedModule } from './shared/shared.module';
+import { MasterLayoutModule } from './layouts/master-layout/master-layout.module';
+import { PlanATripComponent } from './views/plan-a-trip/plan-a-trip.component';
+import { TopbarComponent } from './layouts/master-layout/topbar/topbar.component';
+import { NgxSliderModule } from '@angular-slider/ngx-slider';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AddCarComponent } from './views/plan-a-trip/add-car/add-car.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanATripComponent,
+    TopbarComponent,
+    AddCarComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    MasterLayoutModule,
+    NgxSliderModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
